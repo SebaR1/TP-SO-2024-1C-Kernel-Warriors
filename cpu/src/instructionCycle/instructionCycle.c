a7c28446d0ff98c83dd60508f4ea4180838871dd
Julian Fajardo <julienf0410@gmail.com>
2024-05-12 10:58:10 +0000

    Agregados unos comentarios

--- cpu/src/instructionCycle/instructionCycle.c
+++ cpu/src/instructionCycle/instructionCycle.c
@@ -3,15 +3,18 @@
 
 void runInstructionCycle()
 {
+    // Fase fetch, busco la siguiente instruccion
     memoryInstructionString* instructionString = getNextInstruction(getPC(), socketMemory);
 
+    // Fase decode, descifro qué me pide la instrucción (la cual llegó en forma de string)
     void* instructionParams;
     instructionType type = decodeInstruction(instructionString->instructionString, instructionParams);
 
+    // Ejecuto la instruccion
     executeInstruction(type, instructionParams);
 
 
-
+    // Libero la memoria que usé
     free(instructionParams);
     free(instructionString->instructionString);
     free(instructionString);
