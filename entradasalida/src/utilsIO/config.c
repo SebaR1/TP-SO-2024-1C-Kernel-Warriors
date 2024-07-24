7006c6df78038dc55612c051a2f826b57e64d1ab
Julian Fajardo <julienf0410@gmail.com>
2024-07-24 11:06:34 +0000

    Arreglado un bug en la obtencion de los configs en IO

--- entradasalida/src/utilsIO/config.c
+++ entradasalida/src/utilsIO/config.c
@@ -21,23 +21,32 @@ void _getIOData()
 {
     getIOConfig()->TIPO_INTERFAZ = config_get_string_value(_configFile, "TIPO_INTERFAZ");
 
-    getIOConfig()->TIEMPO_UNIDAD_TRABAJO = config_get_int_value(_configFile, "TIEMPO_UNIDAD_TRABAJO");
+    if (config_has_property(_configFile, "TIEMPO_UNIDAD_TRABAJO"))
+        getIOConfig()->TIEMPO_UNIDAD_TRABAJO = config_get_int_value(_configFile, "TIEMPO_UNIDAD_TRABAJO");
 
-    getIOConfig()->IP_KERNEL = config_get_string_value(_configFile, "IP_KERNEL");
+    if (config_has_property(_configFile, "IP_KERNEL"))
+        getIOConfig()->IP_KERNEL = config_get_string_value(_configFile, "IP_KERNEL");
 
-    getIOConfig()->PUERTO_KERNEL = config_get_string_value(_configFile, "PUERTO_KERNEL");
+    if (config_has_property(_configFile, "PUERTO_KERNEL"))
+        getIOConfig()->PUERTO_KERNEL = config_get_string_value(_configFile, "PUERTO_KERNEL");
 
-    getIOConfig()->IP_MEMORIA = config_get_string_value(_configFile, "IP_MEMORIA");
+    if (config_has_property(_configFile, "IP_MEMORIA"))
+        getIOConfig()->IP_MEMORIA = config_get_string_value(_configFile, "IP_MEMORIA");
 
-    getIOConfig()->PUERTO_MEMORIA = config_get_string_value(_configFile, "PUERTO_MEMORIA");
+    if (config_has_property(_configFile, "PUERTO_MEMORIA"))
+        getIOConfig()->PUERTO_MEMORIA = config_get_string_value(_configFile, "PUERTO_MEMORIA");
 
-    getIOConfig()->PATH_BASE_DIALFS = config_get_string_value(_configFile, "PATH_BASE_DIALFS");
+    if (config_has_property(_configFile, "PATH_BASE_DIALFS"))
+        getIOConfig()->PATH_BASE_DIALFS = config_get_string_value(_configFile, "PATH_BASE_DIALFS");
 
-    getIOConfig()->BLOCK_SIZE = config_get_int_value(_configFile, "BLOCK_SIZE");
+    if (config_has_property(_configFile, "BLOCK_SIZE"))
+        getIOConfig()->BLOCK_SIZE = config_get_int_value(_configFile, "BLOCK_SIZE");
 
-    getIOConfig()->BLOCK_COUNT = config_get_int_value(_configFile, "BLOCK_COUNT");
+    if (config_has_property(_configFile, "BLOCK_COUNT"))
+        getIOConfig()->BLOCK_COUNT = config_get_int_value(_configFile, "BLOCK_COUNT");
 
-    getIOConfig()->RETRASO_COMPACTACION = config_get_int_value(_configFile, "RETRASO_COMPACTACION");
+    if (config_has_property(_configFile, "RETRASO_COMPACTACION"))
+        getIOConfig()->RETRASO_COMPACTACION = config_get_int_value(_configFile, "RETRASO_COMPACTACION");
 }
 
 void initIOConfig(char* path)
